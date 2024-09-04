d6d9f0f3fc1e4569e4a421271c3166e9282c12b4
Kiet T. Tran, Ph.D. <KTran@harrisburgu.edu>
2024-09-04 13:00:28 -0400

    Added new project easy_cuda_1 and updated existing printf example

---
+++ Misc-Projects/easy_cuda_1/add.cu
@@ -0,0 +1,177 @@
+﻿#include <assert.h>
+#include <stdint.h>
+#include <stdio.h>
+
+// CUDA runtime
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+
+// helper functions and utilities to work with CUDA
+#include <helper_cuda.h>
+#include <helper_functions.h>
+
+// cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
+
+#include <iostream>
+#include <math.h>
+// Kernel function to add the elements of two arrays
+__global__
+void add(int n, float* x, float* y)
+{
+    for (int i = 0; i < n; i++)
+        y[i] = x[i] + y[i];
+}
+
+int simple_add(int N)
+{
+    // int N = 1 << 20;
+    float* x, * y;
+
+    // Allocate Unified Memory – accessible from CPU or GPU
+    cudaMallocManaged(&x, N * sizeof(float));
+    cudaMallocManaged(&y, N * sizeof(float));
+
+    // initialize x and y arrays on the host
+    for (int i = 0; i < N; i++) {
+        x[i] = 1.0f;
+        y[i] = 2.0f;
+    }
+
+    // Run kernel on 1M elements on the GPU
+    add << <1, 1 >> > (N, x, y);
+
+    // Wait for GPU to finish before accessing on host
+    cudaDeviceSynchronize();
+
+    // Check for errors (all values should be 3.0f)
+    float maxError = 0.0f;
+    for (int i = 0; i < N; i++)
+        maxError = fmax(maxError, fabs(y[i] - 3.0f));
+    std::cout << "Max error: " << maxError << std::endl;
+
+    // Free memory
+    cudaFree(x);
+    cudaFree(y);
+
+    return 0;
+}
+
+// Kernel function to add the elements of two arrays
+__global__
+void block_add(int n, float* x, float* y)
+{
+    int index = threadIdx.x;
+    int stride = blockDim.x;
+    for (int i = index; i < n; i += stride)
+        y[i] = x[i] + y[i];
+}
+
+int block_add(int N)
+{
+    // int N = 1 << 20;
+    float* x, * y;
+
+    // Allocate Unified Memory – accessible from CPU or GPU
+    cudaMallocManaged(&x, N * sizeof(float));
+    cudaMallocManaged(&y, N * sizeof(float));
+
+    // initialize x and y arrays on the host
+    for (int i = 0; i < N; i++) {
+        x[i] = 1.0f;
+        y[i] = 2.0f;
+    }
+
+    // Run kernel on 1M elements on the GPU
+    block_add << <1, 256 >> > (N, x, y);
+
+    // Wait for GPU to finish before accessing on host
+    cudaDeviceSynchronize();
+
+    // Check for errors (all values should be 3.0f)
+    float maxError = 0.0f;
+    for (int i = 0; i < N; i++)
+        maxError = fmax(maxError, fabs(y[i] - 3.0f));
+    std::cout << "Max error: " << maxError << std::endl;
+
+    // Free memory
+    cudaFree(x);
+    cudaFree(y);
+
+    return 0;
+}
+
+// Kernel function to add the elements of two arrays
+__global__
+void grid_add(int n, float* x, float* y)
+{
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+    for (int i = index; i < n; i += stride)
+        y[i] = x[i] + y[i];
+}
+
+int grid_add(int N)
+{
+    // int N = 1 << 30;
+    float* x, * y;
+
+    // Allocate Unified Memory – accessible from CPU or GPU
+    cudaMallocManaged(&x, N * sizeof(float));
+    cudaMallocManaged(&y, N * sizeof(float));
+
+    // initialize x and y arrays on the host
+    for (int i = 0; i < N; i++) {
+        x[i] = 1.0f;
+        y[i] = 2.0f;
+    }
+
+    // Run kernel on 1M elements on the GPU
+    int blockSize = 256;
+    int numBlocks = (N + blockSize - 1) / blockSize;
+    std::cout << "Number of blocks: " << numBlocks << std::endl;
+    grid_add << <numBlocks, blockSize >> > (N, x, y);
+
+    // Wait for GPU to finish before accessing on host
+    cudaDeviceSynchronize();
+
+    // Check for errors (all values should be 3.0f)
+    float maxError = 0.0f;
+    for (int i = 0; i < N; i++)
+        maxError = fmax(maxError, fabs(y[i] - 3.0f));
+    std::cout << "Max error: " << maxError << std::endl;
+
+    // Free memory
+    cudaFree(x);
+    cudaFree(y);
+
+    return 0;
+}
+
+int main(int argc, const char** argv) {
+    // If the command-line has a device number specified, use it
+    char* app_name = NULL;
+    int N = 1 << 20;
+
+    if (checkCmdLineFlag(argc, argv, "e")) {
+        N = 1 << getCmdLineArgumentInt(argc, argv, "e");
+    }
+    std::cout << "N: " << N << std::endl;
+    if (checkCmdLineFlag(argc, argv, "app")) {
+        getCmdLineArgumentString(argc, argv, "app", &app_name);
+        std::cout << "app=" << app_name << std::endl;
+
+        if (_strnicmp(app_name, "simple_add", strlen(app_name)) == 0) {
+            simple_add(N);
+        } else if (STRNCASECMP(app_name, "block_add", strlen(app_name)) == 0) {
+            block_add(N);
+        } else if (STRNCASECMP(app_name, "grid_add", strlen(app_name)) == 0) {
+            grid_add(N);
+        } else {
+            std::cout << "Invalid app name: " << app_name << std::endl;
+            exit(EXIT_FAILURE);
+        }
+    }
+    else {
+        std::cout << "Must provide -app= parameters!" << std::endl;
+    }
+}
